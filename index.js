ec52a494a01132c0453c5072d45a979e7a813108
kjammes <jayeshkarli@gmail.com>
2021-05-21 19:21:04 +0530

    Made responsive upto 470pxs

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-let main = document.querySelector('main');
-let input = document.getElementById('st');
+let main = document.querySelector("main");
+let input = document.getElementById("st");
 
 let but = document.querySelector("button.add-h1s");
-but.addEventListener('click', () => {
+but.addEventListener("click", () => {
   let h1Tag = document.createElement("h1");
   let text = input.value.toString();
 
-  input.value = '';  
+  input.value = "";
   h1Tag.textContent = text;
+
   main.appendChild(h1Tag);
 });
 
 let but2 = document.querySelector("button.clear-h1s");
-but2.addEventListener('click', () => {
-  let h1Tag = document.querySelector('h1');
+but2.addEventListener("click", () => {
+  let h1Tag = document.querySelector("h1");
 
   main.removeChild(h1Tag);
 });
